1eb4489399193b988352224c1dc0cd2a86133dd4
karthikiki <karthickcks003@gmail.com>
2023-12-01 10:12:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@emotion/react'
+import { useSelector } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import ProtectedRoute from './Components/Authentication/ProtectedRoute'
+import { Home } from './Components/Mainpage/Home'
+import Info from './Components/Mainpage/Info/info'
+import { Urls } from './Components/Mainpage/Urls.js'
+
+
 
 function App() {
+  
+  const Theme = useSelector((state)=>state.theme)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={Theme}>
+        <div className="App">
+          <div style={{margin:"0",padding:'2rem' ,backgroundColor:Theme.palette.background.default ,color:Theme.palette.primary.light}}>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/urls' element={<ProtectedRoute><Urls/></ProtectedRoute>}/>
+            <Route path='/info' element={<Info/>}/>
+          </Routes>
+          </div>
+        </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/url.js
@@ -0,0 +1,4 @@
+
+const API = "http://localhost:5050";
+
+export default API ;
